d45a08ed03bdfc399eb6baf05a66d8db1fc7c00a
Gautam kumar <gautamkumar844506@gmail.com>
2024-03-26 22:38:10 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import React from "react";
+import ComponentA from "./ComponentA"; // ComponentA should start with a capital letter
 
-
-function App() {
-  return (
-   <>
-<h1> Learing life cycle </h1>
-   </>
-  );
+class App extends React.Component {
+  render() {
+    return <ComponentA />; // ComponentA should start with a capital letter
+  }
 }
 
 export default App;
---
+++ src/ComponentA.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class ComponentA extends React.Component { // ComponentA should start with a capital letter
+  constructor() {
+    super();
+    this.state = {
+      name: "ComponentA" // ComponentA should start with a capital letter
+    };
+    console.log("ComponentA constructor");
+  }
+static getDerivedStateFromProps(){
+    console.log("componentA getDriverdstste from props");
+
+}
+componentDidMount (){
+   console.log("componentdidmount")
+}
+  render() {
+    console.log("ComponentA render");
+    return <h1>{this.state.name}</h1>;
+  }
+}
+
+export default ComponentA; // ComponentA should start with a capital letter
