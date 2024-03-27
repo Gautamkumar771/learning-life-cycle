fe3465414227f81266a713e35af13e808035fbd3
Gautam kumar <gautamkumar844506@gmail.com>
2024-03-27 23:40:27 +0530

    componentB added

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { Component } from 'react';
+import ComponentB from './ComponentB';
 
-class ComponentA extends React.Component { // ComponentA should start with a capital letter
-  constructor() {
-    super();
+class ComponentA extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      name: "ComponentA" // ComponentA should start with a capital letter
-    };
-    console.log("ComponentA constructor");
+      name: "ComponentA",
+      data: []
+    }
+    console.log("ComponentA Constructor");
+
   }
-static getDerivedStateFromProps(){
-    console.log("componentA getDriverdstste from props");
+
+  static getDerivedStateFromProps() {
+    console.log("ComponentA getDerivedStateFromProps");
     return null;
+  }
 
-}
-componentDidMount (){
-   console.log("componentdidmount")
+  componentDidMount() {
+    console.log("ComponentA componentDidMount");
+    // this.setState({ name: "ComponentA" });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => {this.setState({data})});
+  }
 
-}
   render() {
-    console.log("ComponentA render");
-    return <h1>{this.state.name}</h1>;
+    console.log("ComponentA Render")
+    return (
+      <>
+        <h1>{this.state.name}</h1>
+        <ul>
+        { this.state.data.map((d) => {
+            return (<li>{d.username}</li>)
+        })}
+        </ul>
+        <ComponentB/> 
+      </>
+    );
   }
 }
 
-export default ComponentA; // ComponentA should start with a capital letter
+export default ComponentA;
